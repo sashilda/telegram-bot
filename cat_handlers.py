90965fea1ee22f8c1c6ed2b22d38217f19b6a656
Alexandra Vlasov <sashilda.vlasov@gmail.com>
2018-09-21 14:19:28 +0300

    Split handler /cat

---
+++ cat_handlers.py
@@ -0,0 +1,10 @@
+import logging
+from glob import glob
+from random import choice
+
+logger = logging.getLogger(__name__)
+
+def send_cat_picture(bot, update, user_data):
+    cat_list = glob('Images/*cat*.jp*g')
+    cat_pic = choice(cat_list)
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
